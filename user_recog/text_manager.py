4c67417fd74da2d8e898c5e0af665699c615712d
Reddinferno <TheReddinferno@gmail.com>
2018-05-10 23:54:35 -0700

    Had some failed  ideas but tried to rework it now.

--- user_recog/text_manager.py
+++ user_recog/text_manager.py
@@ -1,18 +1,40 @@
 import Configure
-from user_recog.mic_listen import ListenLoop
 import grpc
+import sys
+from user_recog.text_sorter import TextSorter
 
 
+# Getting mic stream audio
 class TextManager:
     def __init__(self):
-        # self.text_recognizer = TextRecognizer(
         self.c = Configure
 
-    def get_text_to_speech_google(self, text):
+    @staticmethod
+    def get_text_to_speech_google(responses):
         try:
             print("talk")
             # text_rec
-            ListenLoop.listen_print_loop(self, text)
+            num_chars_printed = 0
+            for response in responses:
+                if not response.results:
+                    continue
+
+                result = response.results[0]
+                if not result.alternatives:
+                    continue
+
+                transcript = result.alternatives[0].transcript
+                overwrite_chars = ' ' * (num_chars_printed - len(transcript))
+
+                if not result.is_final:
+                    sys.stdout.write(transcript + overwrite_chars + '\r')
+                    sys.stdout.flush()
+                    num_chars_printed = len(transcript)
+                else:
+                    get_mic = transcript+overwrite_chars
+                    TextSorter.analyze_speech(get_mic)
+                    num_chars_printed = 0
+
         except grpc.RpcError as e:
             if e.code() not in (grpc.StatusCode.INVALID_ARGUMENT, grpc.StatusCode.OUT_OF_RANGE):
                 raise
